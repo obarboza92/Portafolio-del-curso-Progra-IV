8389fe5850456dfe2188d45c873ac0dce02bc1a9
obarboza92 <70557342+obarboza92@users.noreply.github.com>
2021-11-01 21:34:30 -0600

    Delete Hola_Mundo_TS directory

--- Hola_Mundo_TS/app.js
+++
@@ -1,8 +0,0 @@
-function hola(nombre) {
-    return 'Hola ' + nombre;
-}
-var persona = 'Orlando';
-var message = hola(persona);
-var heading = document.createElement('h1');
-heading.textContent = message;
-document.body.appendChild(heading);
